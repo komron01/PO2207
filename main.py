13a50c546ab2a27cf5430959ee8fc7d5b3eaf276
komron01 <47138499+komron01@users.noreply.github.com>
2023-12-25 09:20:57 +0600

    V0.2
    Здесь я добавил функцию для подключения к онлайн БД, а также вернул AVG

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('My seconds program')
+import psycopg2 
+conn = psycopg2.connect(
+        user="postgres",
+        password="123",
+        host="78.141.227.124",
+        database="GITHUB"
+    )
+
+cur = conn.cursor()
+cur.execute("SELECT ROUND(AVG(age),1) FROM STUDENTS  WHERE stud_name LIKE 'A%'")
+avg= cur.fetchone()
+print('Average:',avg)
