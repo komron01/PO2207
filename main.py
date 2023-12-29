1ce34919fdc1e95b96514e2de1f143a98ee64e8d
unknown <komronis01@gmail.com>
2023-12-29 08:40:01 +0600

    v2.1
    Эта версия от 29.12.2023 для проверки через баш

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import psycopg2 
-conn = psycopg2.connect(
-        user="postgres",
-        password="123",
-        host="78.141.227.124",
-        database="GITHUB"
-    )
-
-cur = conn.cursor()
-cur.execute("SELECT ROUND(AVG(age),1) FROM STUDENTS  WHERE stud_name LIKE 'A%'")
-avg= cur.fetchone()
-print('Average:',avg)
+print('Hello world 3^2=', 3 **2)
